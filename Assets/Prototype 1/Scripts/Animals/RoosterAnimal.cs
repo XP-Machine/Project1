064d8ebf3c8cb639f268de6483268cdd3668cb92
Laurent Klein <klela095@hotmail.com>
2024-10-25 10:58:43 +0200

    Chicken Gliding Implemented
    Chicken gliding implemented
    Added Laurent Scene to avoid merging troubles

--- Assets/Prototype 1/Scripts/Animals/RoosterAnimal.cs
+++ Assets/Prototype 1/Scripts/Animals/RoosterAnimal.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class RoosterAnimal : CharacterBase
 {
     public float interactionRadius = 1;
+    public float gravityReductionRate = 0.3f;
+    public bool isGliding = false;
     public void Start()
     {
         base.Start();
         animalType = StackManager.Animal.Rooster;
         AnimalTags = new string[] { "Cat", "Dog", "Donkey" };
     }
+    
+
     public override void Move(Vector2 inputVect)
     {
+        if (isGliding)
+        {
+            gravity *= gravityReductionRate;
+        }
+        
         base.Move(inputVect);
+
+        gravity = -9.81f;
+        if (isGrounded)
+        {
+            isGliding = false;
+        }
     }
+    
     public override void AbilityX()
     {
         base.AbilityX();
@@ -28,6 +44,16 @@ public class RoosterAnimal : CharacterBase
             }
         }
     }
+
+    public override void AbilityY()
+    {
+        base.AbilityY();
+        if (!isGrounded)
+        {
+            isGliding = true;
+        }
+    }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
